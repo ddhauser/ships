d5d8ac9b8c4a54b317534a3e57251abbfa787133
ddhauser <dd.hauser@gmail.com>
2017-09-19 00:17:55 +0300

    linting

--- src/client/components/shipsMap/shipsMap.component.js
+++ src/client/components/shipsMap/shipsMap.component.js
@@ -1,81 +1,81 @@
 angular.module('app').component('shipsMap', {
-  template: '<div class="map"></div>',
-  bindings: {
-    onMapLoaded: '&?',
-    options: '<?', /* A google.maps.MapOptions object */
-  },
-  controller(API_ENDPOINT, $rootScope, $element, googleMapsAPI, shipsAPI, $compile) {
+    template: '<div class="map"></div>',
+    bindings: {
+        onMapLoaded: '&?',
+        /* A google.maps.MapOptions object */
+        options: '<?',
+    },
+    controller(API_ENDPOINT, $rootScope, $element, googleMapsAPI, shipsAPI, $compile) {
+        function generateShipIcon(feature) {
+            const defaultIcon = {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                strokeColor: 'black',
+                fillColor: '#00F',
+                strokeWeight: 2,
+                scale: 4,
+            };
 
-    function generateShipIcon(feature) {
-      const defaultIcon = {
-        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-        strokeColor: 'black',
-        fillColor: '#00F',
-        strokeWeight: 2,
-        scale: 4,
-      };
+            return {
+                icon: angular.extend(defaultIcon, {
+                    // Add any relevant feature propertis (if any)
+                    rotation: feature.getProperty('course') || defaultIcon.rotation,
+                    strokeColor: feature.getProperty('strokeColor') || defaultIcon.strokeColor,
+                    scale: feature.getProperty('scale') || defaultIcon.scale,
+                }),
+            };
+        }
 
-      return {
-        icon: angular.extend(defaultIcon, {
-          // Add any relevant feature propertis (if any)
-          rotation: feature.getProperty('course') || defaultIcon.rotation,
-          strokeColor: feature.getProperty('strokeColor') || defaultIcon.strokeColor,
-          scale: feature.getProperty('scale') || defaultIcon.scale,
-        }),
-      };
-    }
+        const createMap = () => googleMapsAPI.get().then((google) => {
+            const mapElem = $element.find('div')[0];
 
-    const createMap = () => googleMapsAPI.get().then((google) => {
-      const mapElem = $element.find('div')[0];
+            // Create the map
+            const map = new google.maps.Map(mapElem, angular.extend({
+                disableDefaultUI: true,
+            }, this.options));
 
-      // Create the map
-      const map = new google.maps.Map(mapElem, angular.extend({
-        disableDefaultUI: true,
-      }, this.options));
+            // Fire event
+            (this.onMapLoaded || angular.noop)({
+                $map: map,
+            });
 
-      // Fire event
-      (this.onMapLoaded || angular.noop)({
-        $map: map,
-      });
+            return map;
+        });
 
-      return map;
-    });
+        const createInfoWindow = (map) => {
+            const infowindow = new google.maps.InfoWindow();
 
-    const createInfoWindow = (map) => {
-      const infowindow = new google.maps.InfoWindow();
+            // Open the infoWindow when a feature is clicked
+            map.data.addListener('click', (event) => {
+                console.debug('clicked:', event.feature);
 
-      // Open the infoWindow when a feature is clicked
-      map.data.addListener('click', (event) => {
-        console.debug('clicked:', event.feature);
+                const shipData = $compile(`<ship-data ship="${event.feature.getId()}">`)($rootScope)[0];
 
-        const shipData = $compile(`<ship-data ship="${event.feature.getId()}">`)($rootScope)[0];
+                infowindow.setContent(shipData);
+                infowindow.setPosition(event.feature.getGeometry().get());
+                infowindow.open(map);
+            });
 
-        infowindow.setContent(shipData);
-        infowindow.setPosition(event.feature.getGeometry().get());
-        infowindow.open(map);
-      });
+            map.addListener('click', () => {
+                infowindow.close();
+            });
+        };
 
-      map.addListener('click', () => {
-        infowindow.close();
-      });
-    };
+        const loadPointsToDataLayer = (data) => {
+            // Clear all data
+            data.forEach(feature => data.remove(feature));
 
-    const loadPointsToDataLayer = (data) => {
-      // Clear all data
-      data.forEach(feature => data.remove(feature));
+            // Add all ships
+            data.loadGeoJson(`${API_ENDPOINT}/points`);
+        };
 
-      // Add all ships
-      data.loadGeoJson(`${API_ENDPOINT}/points`);
-    };
+        this.$onInit = () => {
+            console.debug('$onInit ships-map', $element);
 
-    this.$onInit = () => {
-      console.debug('$onInit ships-map', $element);
-
-      createMap().then((map) => {
-        createInfoWindow(map);
-        loadPointsToDataLayer(map.data);
-        map.data.setStyle(generateShipIcon);
-      });
-    };
-  },
+            createMap().then((map) => {
+                createInfoWindow(map);
+                loadPointsToDataLayer(map.data);
+                map.data.setStyle(generateShipIcon);
+            });
+        };
+    },
 });
